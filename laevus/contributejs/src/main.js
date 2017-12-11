f0c76bb18948376c30908bfdfa04eb5c0446d4fd
Yg Versil <ygversil@openmailbox.org>
2017-12-11 22:54:40 +0100

    [vue] Add Buefy components
    Buefy is a collection of components for Vue.js based on Bulma.
    
    We use the font-awesome icon collections.

--- laevus/contributejs/src/main.js
+++ laevus/contributejs/src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import App from './App'
+import Buefy from 'buefy'
 import Vue from 'vue'
 import router from './router'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
 
 /* eslint-disable no-new */
 new Vue({
