5cc83a944220f490dc838064fcb97cb66e42b009
Yg Versil <ygversil@openmailbox.org>
2017-12-13 16:05:15 +0100

    [vue] Add vue-form-wizard

--- laevus/contributejs/src/main.js
+++ laevus/contributejs/src/main.js
@@ -4,6 +4,7 @@ import App from './App'
 import Buefy from 'buefy'
 import Vue from 'vue'
 import Vue2Leaflet from 'vue2-leaflet'
+import VueFormWizard from 'vue-form-wizard'
 import router from './router'
 import 'buefy/lib/buefy.css'
 
@@ -16,6 +17,7 @@ Vue.component('v-marker', Vue2Leaflet.Marker)
 Vue.use(Buefy, {
   defaultIconPack: 'fa'
 })
+Vue.use(VueFormWizard)
 
 /* eslint-disable no-new */
 new Vue({
