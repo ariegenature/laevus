51901e4f222b8a8ccfb5c969d57225a754d07ca8
Yg Versil <ygversil@openmailbox.org>
2017-12-13 12:02:36 +0100

    [vue] Add leaflet map on home page
    Values (zoom, center, coordinates) are coming straight from vue2-leaflet
    example in github.

--- laevus/contributejs/src/components/Home.vue
+++ laevus/contributejs/src/components/Home.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-  </div>
+  <section class="hero is-fullheight">
+    <div id="hero-map" class="hero-body">
+      <div id="container-map" class="container">
+        <div id="map">
+          <v-map :zoom="13" :center="[47.413220, -1.219482]">
+            <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                         attribution="OpenStreetMap contributors"></v-tilelayer>
+            <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+          </v-map>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Home',
   data () {
-    return {
-    }
   }
 }
 </script>
+
+<style>
+#hero-map, #container-map {
+  height: 100%;
+}
+#map {
+  height: 90%;
+}
+</style>
