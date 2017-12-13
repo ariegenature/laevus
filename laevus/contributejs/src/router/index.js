c6d857fe1dbae827d48b600ca0a08326d9e98636
Yg Versil <ygversil@openmailbox.org>
2017-12-13 15:03:33 +0100

    [vue] Make vue.js route match Flask route
    That is `https://example.org/contribute` should call the `Home` componenent of
    the `contributejs` client.

--- laevus/contributejs/src/router/index.js
+++ laevus/contributejs/src/router/index.js
@@ -8,9 +8,13 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/contribute',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/',
+      redirect: '/contribute'
     }
   ]
 })
