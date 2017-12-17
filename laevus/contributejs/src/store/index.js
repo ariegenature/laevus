fc572341cac69b4112918daa4bd869a61d2fa302
Yg Versil <ygversil@openmailbox.org>
2017-12-18 00:18:09 +0100

    [vue] Add animal groups in store
    Each group has an `id`, a `name` and a URL to an `icon`.
    
    And move their icons into Flask static directory so that they can
    programmatically be referenced in `src` attribute of an `img` tag.

--- laevus/contributejs/src/store/index.js
+++ laevus/contributejs/src/store/index.js
@@ -6,6 +6,45 @@ import contribution from './contribution'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state () {
+    return {
+      groups: [
+        {
+          id: 'large-mammal',
+          name: 'Grand mammifère',
+          icon: 'static/deer.png'
+        },
+        {
+          id: 'small-mammal',
+          name: 'Petit mammifère',
+          icon: 'assets/deer.png'
+        },
+        {
+          id: 'reptile',
+          name: 'Reptile',
+          icon: 'assets/deer.png'
+        },
+        {
+          id: 'amphibian',
+          name: 'Amphibien',
+          icon: 'assets/deer.png'
+        },
+        {
+          id: 'bat',
+          name: 'Chauve-souris',
+          icon: 'assets/deer.png'
+        },
+        {
+          id: 'bird',
+          name: 'Oiseau',
+          icon: 'assets/deer.png'
+        }
+      ]
+    }
+  },
+  getters: {
+    groups: (state) => state.groups
+  },
   modules: {
     contribution
   }
