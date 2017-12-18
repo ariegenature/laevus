10cc232c52f47326742ddd44c491168e7eba513a
Yg Versil <ygversil@openmailbox.org>
2017-12-18 22:18:21 +0100

    [vue] Move Leaflet map into its own component

---
+++ laevus/contributejs/src/components/ContributeMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-map :zoom="13" :center="[47.413220, -1.219482]" @l-click="transmitClick"
+    class="locate">
+    <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                 attribution="OpenStreetMap contributors"></v-tilelayer>
+    <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+  </v-map>
+</template>
+
+<script>
+export default {
+  name: 'ContributeMap',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    transmitClick (ev) {
+      this.$emit('l-click')
+    }
+  }
+}
+</script>
+
+<style>
+.leaflet-container.locate {
+  cursor: crosshair;
+}
+</style>
--- laevus/contributejs/src/components/Home.vue
+++ laevus/contributejs/src/components/Home.vue
@@ -3,12 +3,7 @@
     <div id="hero-map" class="hero-body">
       <div id="container-map" class="container">
         <div id="map">
-          <v-map :zoom="13" :center="[47.413220, -1.219482]" @l-click="handleMapClick"
-             class="locate">
-            <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                         attribution="OpenStreetMap contributors"></v-tilelayer>
-            <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
-          </v-map>
+          <contribute-map @l-click="handleMapClick"></contribute-map>
         </div>
       </div>
     </div>
@@ -20,11 +15,13 @@
 
 <script>
 import ContributeForm from './ContributeForm'
+import ContributeMap from './ContributeMap'
 
 export default {
   name: 'Home',
   components: {
-    ContributeForm
+    ContributeForm,
+    ContributeMap
   },
   data () {
     return {
