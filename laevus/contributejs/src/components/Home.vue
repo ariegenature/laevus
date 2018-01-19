ba1e533be9358af2d0960871ac2431cb194b3bbe
Yg Versil <ygversil@openmailbox.org>
2018-01-19 13:14:28 +0100

    [vue] Put map help in a collapsable card
    And below the map

--- laevus/contributejs/src/components/Home.vue
+++ laevus/contributejs/src/components/Home.vue
@@ -7,24 +7,32 @@
             <div id="map">
               <contribute-map @perimeter-click="handleMapClick"></contribute-map>
             </div>
+            <b-collapse class="card is-small" :open.sync="isHelpShown">
+              <div slot="trigger" class="card-header">
+                <p class="card-header-title is-size-7">Comment utiliser la carte ?</p>
+                <a class="card-header-icon"><b-icon :icon="caret"></b-icon></a>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <p class="subtitle is-6 has-text-weight-bold">Ajouter une observation</p>
+                  <p class="is-size-7">Pour saisir une nouvelle observation, il suffit de cliquer à
+                  l'endroit où vous avez observé un animal.</p>
+                  <p class="is-size-7">Afin de garantir un minimum de précision, le niveau de zoom
+                  de la carte doit être suffisant.</p>
+                  <p class="subtitle is-6 has-text-weight-bold">Voir les observations</p>
+                  <p class="is-size-7">Sur la carte apparaissent les observations
+                  <em>déjà saisies</em> sous forme de points.</p>
+                  <p class="is-size-7">Cliquez sur un point pour voir le détail.</p>
+                </div>
+              </div>
+            </b-collapse>
           </div>
           <div class="column">
             <div class="content">
-              <h3 class="is-size-5">Comment utiliser la carte ?</h3>
-              <h2 class="is-size-6">Ajouter une observation</h2>
               <p class="is-size-7"><b-taglist attached>
                 <b-tag class="is-dark">niveau de zoom</b-tag>
                 <b-tag :class="[tagClass]">{{ tagText }}</b-tag>
               </b-taglist>
-              <p class="is-size-7">Pour saisir une nouvelle observation, il suffit de cliquer à
-              l'endroit où vous avez observé un animal.</p>
-              <p class="is-size-7">Afin de garantir un minimum de précision, le niveau de zoom de
-              la carte doit être suffisant.</p>
-              </p>
-              <h2 class="is-size-6">Voir les observations</h2>
-              <p class="is-size-7">Sur la carte apparaissent les observations
-              <em>déjà saisies</em> sour forme de points.</p>
-              <p class="is-size-7">Cliquez sur un point pour voir le détail.</p>
             </div>
           </div>
         </div>
@@ -52,10 +60,14 @@ export default {
   },
   data () {
     return {
-      isFormActive: false
+      isFormActive: false,
+      isHelpShown: false
     }
   },
   computed: {
+    caret () {
+      return this.isHelpShown ? 'caret-up' : 'caret-down'
+    },
     tagClass () {
       return this.zoom >= 14 ? 'is-success' : 'is-danger'
     },
