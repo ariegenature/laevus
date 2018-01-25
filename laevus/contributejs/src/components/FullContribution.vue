32d422341cc2dd8b2d1be37e1500e8ca0aefcd44
Yg Versil <ygversil@openmailbox.org>
2018-01-25 15:13:22 +0100

    [vue] Add button to download Shapefile

--- laevus/contributejs/src/components/FullContribution.vue
+++ laevus/contributejs/src/components/FullContribution.vue
@@ -4,6 +4,11 @@
       <div class="container is-fluid">
         <div class="columns">
           <div class="column is-centered">
+            <div class="has-text-right">
+              <a class="button is-primary" href="/full-contribution/download/shp">
+                Télécharger au format ESRI Shapefile
+              </a>
+            </div>
             <full-contribution-table></full-contribution-table>
           </div>
         </div>
