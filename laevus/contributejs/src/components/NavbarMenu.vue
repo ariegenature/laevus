f05264c4071563f180b787d6edfcc96efe25ec58
Yg Versil <ygversil@openmailbox.org>
2018-01-25 16:05:37 +0100

    [vue] Put icon to the left of nav link

--- laevus/contributejs/src/components/NavbarMenu.vue
+++ laevus/contributejs/src/components/NavbarMenu.vue
@@ -3,21 +3,21 @@
     <div class="navbar-start">
       <router-link :to="{ name: 'home' }" class="navbar-item" active-class="is-active"
                    v-if="isAuthenticated" @click.native="emitMenuClick">
-        Les contributions&nbsp;<b-icon icon="map-marker"></b-icon>
+        <b-icon icon="map-marker"></b-icon>&nbsp;Les contributions
       </router-link>
       <router-link :to="{ name: 'full-contribution' }" class="navbar-item" active-class="is-active"
                    v-if="isAuthenticated" @click.native="emitMenuClick">
-        Tableau détaillé&nbsp;<b-icon icon="lock"></b-icon>
+        <b-icon icon="table"></b-icon>&nbsp;Tableau détaillé
       </router-link>
     </div>
     <div class="navbar-end">
       <router-link :to="{ name: 'login' }" class="navbar-item" v-if="!isAuthenticated"
                    @click.native="emitMenuClick">
-        Connexion&nbsp;<b-icon icon="sign-in"></b-icon>
+        <b-icon icon="sign-in"></b-icon>&nbsp;Connexion
       </router-link>
       <span class="navbar-item" v-if="isAuthenticated">{{ displayName }}</span>
       <a  href="/logout" class="navbar-item" v-if="isAuthenticated" @click.native="emitMenuClick">
-        Déconnexion&nbsp;<b-icon icon="sign-out"></b-icon>
+        <b-icon icon="sign-out"></b-icon>&nbsp;Déconnexion
       </a>
     </div>
   </div>
