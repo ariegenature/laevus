311d0ef51a52ec0a4c0facb4f9f09e337413a9a8
Yg Versil <ygversil@openmailbox.org>
2018-02-06 12:23:43 +0100

    [vue] Add first version of leaflet-draw component
    No props or FeatureGroup layer at the moment, just check if toolbar appears
    correctly.

---
+++ laevus/contributejs/src/components/LeafletDraw.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+import 'leaflet-draw'
+import 'leaflet-draw/dist/leaflet.draw.css'
+
+export default {
+  name: 'LeafletDraw',
+  mounted () {
+    this.mapObject = new L.Control.Draw()
+    if (this.$parent._isMounted) {
+      this.deferredMountedTo(this.$parent.mapObject)
+    }
+  },
+  methods: {
+    deferredMountedTo (parent) {
+      this.parent = parent
+      parent.addControl(this.mapObject)
+    }
+  }
+}
+</script>
