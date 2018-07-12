926e6803b8f4c133758e38da74475844b1c60b8f
Yg Versil <ygversil@openmailbox.org>
2018-07-12 13:43:24 +0200

    [js] Add first props to locate control component

--- laevus/contributejs/src/components/LeafletLocateControl.vue
+++ laevus/contributejs/src/components/LeafletLocateControl.vue
@@ -9,10 +9,64 @@ import L from 'leaflet'
 import 'leaflet.locatecontrol'
 import 'leaflet.locatecontrol/dist/L.Control.Locate.css'
 
+const props = {
+  position: {
+    type: String,
+    default: 'topleft'
+  },
+  markerClass: {
+    type: Function,
+    default: L.CircleMarker
+  },
+  icon: {
+    type: String,
+    default: 'fa fa-bullseye'
+  },
+  showPopup: {
+    type: Boolean,
+    default: true
+  },
+  controlHelp: {
+    type: String,
+    default: 'Où suis-je ?'
+  },
+  metersUnit: {
+    type: String,
+    default: 'mètres'
+  },
+  feetUnit: {
+    type: String,
+    default: 'pieds'
+  },
+  popupMessage: {
+    type: String,
+    default: 'Vous êtes à {distance} {unit} de ce point'
+  },
+  outsideMapBoundsMsg: {
+    type: String,
+    default: 'Votre position semble être en dehors des limites de la carte'
+  }
+}
+
 export default {
   name: 'LeafletLocateControl',
+  props,
   mounted () {
-    this.mapObject = new L.Control.Locate()
+    var locateOptions = {}
+    Object.assign(locateOptions, {
+      position: this.position,
+      markerClass: this.markerClass,
+      icon: this.icon,
+      showPopup: this.showPopup,
+      strings: {
+        title: this.controlHelp,
+        metersUnit: this.metersUnit,
+        feetUnit: this.feetUnit,
+        popup: this.popupMessage,
+        outsideMapBoundsMsg: this.outsideMapBoundsMsg
+      }
+    })
+    this.mapObject = new L.Control.Locate(locateOptions)
     if (this.$parent._isMounted) {
       this.deferredMountedTo(this.$parent.mapObject)
     }
