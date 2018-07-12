6c868ed26396796ea1b79a94a334737f0e917140
Yg Versil <ygversil@openmailbox.org>
2018-07-12 13:02:35 +0200

    [js] First version of a leaflet.locatecontrol component

---
+++ laevus/contributejs/src/components/LeafletLocateControl.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+import 'leaflet.locatecontrol'
+import 'leaflet.locatecontrol/dist/L.Control.Locate.css'
+
+export default {
+  name: 'LeafletLocateControl',
+  mounted () {
+    this.mapObject = new L.Control.Locate()
+    if (this.$parent._isMounted) {
+      this.deferredMountedTo(this.$parent.mapObject)
+    }
+  },
+  methods: {
+    deferredMountedTo (parent) {
+      this.parent = parent
+      parent.addControl(this.mapObject)
+    }
+  }
+}
+</script>
