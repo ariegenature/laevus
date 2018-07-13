b53d904f893f4f879c99464c216c53f228db912d
Yg Versil <ygversil@openmailbox.org>
2018-07-13 15:12:43 +0200

    Reduce logo size
    And make it square so that it can be used as favicon.

--- laevus/contributejs/src/components/NavbarBrand.vue
+++ laevus/contributejs/src/components/NavbarBrand.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar-brand">
     <a class="navbar-item" href="/">
-      <img src="static/logo.png" alt="laevus" width="144" height="48">
+      <img src="static/logo.png" alt="laevus" height="48">
     </a>
     <div class="button navbar-burger" :class="{ 'is-active': menuActive }"
          @click="emitBurgerClick">
